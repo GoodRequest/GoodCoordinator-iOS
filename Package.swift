44038831b842548c41a69dbc124da1dbd1d07fb0
Filip Sasala <31418257+plajdo@users.noreply.github.com>
2025-09-25 01:24:50 +0200

    fix: Migrate to new swift-syntax package

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/goodrequest/GoodReactor.git", .upToNextMajor(from: "2.2.0")),
         .package(url: "https://github.com/apple/swift-collections.git", .upToNextMajor(from: "1.1.3")),
-        .package(url: "https://github.com/apple/swift-syntax.git", .upToNextMajor(from: "602.0.0")),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", "600.0.0" ..< "603.0.0"),
         .package(url: "https://github.com/pointfreeco/swift-macro-testing.git", from: "0.6.4")
     ],
     targets: [
